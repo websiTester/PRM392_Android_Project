2dc622cad3e0cdda6d0303a6f724a2d7424b2fb5
TruongKDHE181747 <truongkdhe181747@fpt.edu.vn>
2025-10-18 11:31:46 +0700

    Add splash

---
+++ app/src/main/java/com/example/prm392_android_project/views/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.example.prm392_android_project.views;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.prm392_android_project.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+    }
+}
