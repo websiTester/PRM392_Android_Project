7aa20f5657d34cf66186fbc0713d7ca7f1e24ec8
TruongKDHE181747 <truongkdhe181747@fpt.edu.vn>
2025-10-26 19:48:07 +0700

    add camera, library

---
+++ app/src/main/java/com/example/prm392_android_project/views/LoginSuccessActivity.java
@@ -0,0 +1,22 @@
+package com.example.prm392_android_project.views;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.prm392_android_project.R;
+
+public class LoginSuccessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login_success);
+
+    }
+}
