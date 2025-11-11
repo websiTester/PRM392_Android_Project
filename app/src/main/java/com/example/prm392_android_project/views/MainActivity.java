14b038de4de158ac1fe4ccd1d78a12a70511df1d
TruongKDHE181747 <truongkdhe181747@fpt.edu.vn>
2025-11-11 22:44:48 +0700

    update login service

--- app/src/main/java/com/example/prm392_android_project/views/MainActivity.java
+++ app/src/main/java/com/example/prm392_android_project/views/MainActivity.java
@@ -49,23 +49,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.test_assignment_detail).setOnClickListener(this);
+
         mCompositeDisposable = new CompositeDisposable();
         viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
         Intent intent = getIntent();
         int classId = intent.getIntExtra("classId", 0);
         assignmentId = intent.getIntExtra("assignmentId", 0);
         viewModel.setClassId(classId);
+        int groupId = viewModel.getGroupId();
+        AssignmentDetailFragment fragment = AssignmentDetailFragment.newInstance(groupId,assignmentId);
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, fragment).commit();
     }
 
     @Override
     public void onClick(View view) {
-        if (view.getId() == R.id.test_assignment_detail) {
-            view.findViewById(R.id.test_assignment_detail).setVisibility(GONE);
-            int groupId = viewModel.getGroupId();
-            AssignmentDetailFragment fragment = AssignmentDetailFragment.newInstance(groupId,assignmentId);
-            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, fragment).commit();
-        }
+
     }
 
     @Override
